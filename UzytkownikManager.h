b64ad084f6857ec66ce5ec787dc308f5149a3c67
Lukasz Gorczyk <lukasz.gorczyk.programista@gmail.com>
2020-11-12 00:08:57 +0100

    Divide KsiazkaAdresowa to smaller class

---
+++ UzytkownikManager.h
@@ -0,0 +1,29 @@
+#ifndef UZYTKOWNIKMANAGER_H
+#define UZYTKOWNIKMANAGER_H
+
+#include <iostream>
+#include <vector>
+#include <windows.h>
+#include <fstream>
+#include <sstream>
+#include "Uzytkownik.h"
+#include "PlikZUzytkownikami.h"
+
+using namespace std;
+
+class UzytkownikManager{
+    int idZalogowanegoUzytkownika;
+    vector <Uzytkownik> uzytkownicy;
+    PlikZUzytkownikami plikZUzytkownikami;
+
+    Uzytkownik podajDaneNowegoUzytkownika();
+    int pobierzIdNowegoUzytkownika();
+    bool czyIstniejeLogin(string login);
+
+public:
+    void rejestracjaUzytkownika();
+    void wypiszWszystkichUzytkownikow();
+};
+
+#endif // UZYTKOWNIKMANAGER_H
+
