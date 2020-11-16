13f2cc9ebb52e39fce4dc17fd8b87ed609b5a8be
Lukasz Gorczyk <lukasz.gorczyk.programista@gmail.com>
2020-11-16 23:21:47 +0100

    added menu

--- main.cpp
+++ main.cpp
@@ -3,39 +3,62 @@
 
 using namespace std;
 
-int main()
-{
+int main() {
     KsiazkaAdresowa ksiazkaAdresowa("Uzytkownicy.txt", "Adresaci.txt");
+    char wybor;
 
-    ksiazkaAdresowa.wypiszWszystkichUzytkownikow();
+    while (true) {
+        if (ksiazkaAdresowa.czyUzytkownikJestZalogowany() == 0) {
+            wybor = ksiazkaAdresowa.wybierzOpcjeZMenuGlownego();
 
-    ksiazkaAdresowa.logowanieUzytkownika();
+            switch (wybor) {
+            case '1':
+                ksiazkaAdresowa.rejestracjaUzytkownika();
+                break;
+            case '2':
+                ksiazkaAdresowa.logowanieUzytkownika();
+                break;
+            case '9':
+                exit(0);
+                break;
+            default:
+                cout << endl << "Nie ma takiej opcji w menu." << endl << endl;
+                system("pause");
+                break;
+            }
+        } else {
 
-    ksiazkaAdresowa.wyswietlWszystkichAdresatow();
+            wybor = ksiazkaAdresowa.wybierzOpcjeZMenuUzytkownika();
 
-    ksiazkaAdresowa.dodajAdresata();
-
-    ksiazkaAdresowa.wyswietlWszystkichAdresatow();
-
-    ksiazkaAdresowa.wylogowanieUzytkownika();
-
-    ksiazkaAdresowa.logowanieUzytkownika();
-
-    ksiazkaAdresowa.wyswietlWszystkichAdresatow();
-
-    ksiazkaAdresowa.wylogowanieUzytkownika();
-
-    ksiazkaAdresowa.logowanieUzytkownika();
-
-    ksiazkaAdresowa.dodajAdresata();
-
-    ksiazkaAdresowa.dodajAdresata();
-
-    ksiazkaAdresowa.wyswietlWszystkichAdresatow();
-
-    ksiazkaAdresowa.wylogowanieUzytkownika();
-
-    ksiazkaAdresowa.wyswietlWszystkichAdresatow();
-
+            switch (wybor) {
+            case '1':
+                ksiazkaAdresowa.dodajAdresata();
+                break;
+            case '2':
+                ;
+                break;
+            case '3':
+                ;
+                break;
+            case '4':
+                ksiazkaAdresowa.wyswietlWszystkichAdresatow();
+                break;
+            case '5':
+                ;
+                break;
+            case '6':
+                ;
+                break;
+            case '7':
+                ksiazkaAdresowa.zmianaHaslaZalogowanegoUzytkownika();
+                break;
+            case '8':
+                ksiazkaAdresowa.wylogowanieUzytkownika();
+                break;
+            }
+        }
+    }
     return 0;
 }
+
+
